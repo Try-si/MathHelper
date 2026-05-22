a238483f3d1e22e6a5c2a7429a0596f301b30b5b
Try_si <ezio@elendil.fr>
2026-05-22 17:31:13 +0200

    modified:   Math/MathV2.go

--- Math/MathV2.go
+++ Math/MathV2.go
@@ -10,3 +10,7 @@ func V2Normalize(V2 [2]float64) [2]float64 {
 func V2Distance(V2a [2]float64, V2b [2]float64) float64 {
 	return math.Sqrt((V2a[0]-V2b[0])*(V2a[0]-V2b[0]) + (V2a[1]-V2b[1])*(V2a[1]-V2b[1]))
 }
+
+func V2Length(V2 [2]float64) float64 {
+	return math.Sqrt(V2[0]*V2[0] + V2[1]*V2[1])
+}
