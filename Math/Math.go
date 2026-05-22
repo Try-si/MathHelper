106f0cf9a62b98a4bd96022861b6b4f6aaeed1e2
Try_si <ezio@elendil.fr>
2026-05-22 16:21:50 +0200

    modified:   Math/Math.go

--- Math/Math.go
+++ Math/Math.go
@@ -1,5 +1,8 @@
 package math
 
-func V2Normilize(V2 [2]int) [2]int {
-	return [2]int{V2[0] / (V2[0]*V2[0] + V2[1]*V2[1]), V2[1] / (V2[0]*V2[0] + V2[1]*V2[1])}
+import "math"
+
+func V2Normalize(V2 [2]float64) [2]float64 {
+	magnitude := math.Sqrt(V2[0]*V2[0] + V2[1]*V2[1])
+	return [2]float64{V2[0] / magnitude, V2[1] / magnitude}
 }
